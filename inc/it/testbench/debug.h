d6698b962f81129dd446f037da183dbb4773f309
Raffaele Aquilone <aquiloneraf@gmail.com>
2013-05-09 18:17:02 +0200

    Added assert() macros and improved debug()
    New assert() macros have been added to debug.h, they print either
    PASS or FAIL for the sake of Unit Testing modules.
    Now debug() also prints file name and line number.
    
    Signed-off-by: Raffaele Aquilone <aquiloneraf@gmail.com>

--- inc/it/testbench/debug.h
+++ inc/it/testbench/debug.h
@@ -6,9 +6,33 @@
 //#define DEBUG_PRINT_ENABLED   1
 
 #if DEBUG_PRINT_ENABLED
-#define DEBUG(fmt) (::std::cout <<fmt <<::std::endl)
+
+/**
+ * Define the whole set of debug and assert macros
+ */
+
+#define DEBUG(fmt) \
+    (::std::cout <<__FILE__ <<":" <<__func__ <<":" <<__LINE__ <<"  " <<fmt <<::std::endl)
+#define ASSERT_EQUAL(a, b) \
+    DEBUG(( ((a) == (b)) ? "    PASS" : "    * FAIL *" ))
+#define ASSERT_NOT_EQUAL(a, b) \
+    DEBUG(( ((a) != (b)) ? "    PASS" : "    * FAIL *" ))
+#define ASSERT_STRING_EQUAL(a, b) \
+    DEBUG(( ((a).compare(b) == 0) ? "    PASS" : "    * FAIL *" ))
+#define ASSERT_STRING_NOT_EQUAL(a, b) \
+    DEBUG(( ((a).compare(b) != 0) ? "    PASS" : "    * FAIL *" ))
+
 #else
+
+/**
+ * Debug and assert macros will be void
+ */
+
 #define DEBUG(fmt)
-#endif
+#define ASSERT_EQUAL(a, b)
+#define ASSERT_NOT_EQUAL(a, b)
+#define ASSERT_STRING_NOT_EQUAL(a, b)
+
+#endif /* DEBUG_PRINT_ENABLED */
 
 #endif /* DEBUG_H_ */

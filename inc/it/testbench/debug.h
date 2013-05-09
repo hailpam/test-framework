ac3e131a305e157c76aa9c03f7fb615bed5bb191
Raffaele Aquilone <aquiloneraf@gmail.com>
2013-05-09 21:29:40 +0200

    Differentiated debug() from print() and assert()
    Made a difference between debug(), that is enabled by the
    specific compilation flag, and the print() and assert()
    macros that are always enabled and provide the user with
    less verbose informations.
    
    Signed-off-by: Raffaele Aquilone <aquiloneraf@gmail.com>

--- inc/it/testbench/debug.h
+++ inc/it/testbench/debug.h
@@ -6,33 +6,25 @@
 //#define DEBUG_PRINT_ENABLED   1
 
 #if DEBUG_PRINT_ENABLED
-
-/**
- * Define the whole set of debug and assert macros
- */
-
 #define DEBUG(fmt) \
     (::std::cout <<__FILE__ <<":" <<__func__ <<":" <<__LINE__ <<"  " <<fmt <<::std::endl)
-#define ASSERT_EQUAL(a, b) \
-    DEBUG(( ((a) == (b)) ? "    PASS" : "    * FAIL *" ))
-#define ASSERT_NOT_EQUAL(a, b) \
-    DEBUG(( ((a) != (b)) ? "    PASS" : "    * FAIL *" ))
-#define ASSERT_STRING_EQUAL(a, b) \
-    DEBUG(( ((a).compare(b) == 0) ? "    PASS" : "    * FAIL *" ))
-#define ASSERT_STRING_NOT_EQUAL(a, b) \
-    DEBUG(( ((a).compare(b) != 0) ? "    PASS" : "    * FAIL *" ))
-
 #else
+#define DEBUG(fmt)
+#endif /* DEBUG_PRINT_ENABLED */
 
 /**
- * Debug and assert macros will be void
+ * Define the whole set of print and assert macros
  */
+#define PRINT(fmt) \
+    (::std::cout <<fmt <<::std::endl)
+#define ASSERT_EQUAL(a, b) \
+    PRINT(( ((a) == (b)) ? "    PASS" : "    * FAIL *" ))
+#define ASSERT_NOT_EQUAL(a, b) \
+    PRINT(( ((a) != (b)) ? "    PASS" : "    * FAIL *" ))
+#define ASSERT_STRING_EQUAL(a, b) \
+    PRINT(( ((a).compare(b) == 0) ? "    PASS" : "    * FAIL *" ))
+#define ASSERT_STRING_NOT_EQUAL(a, b) \
+    PRINT(( ((a).compare(b) != 0) ? "    PASS" : "    * FAIL *" ))
 
-#define DEBUG(fmt)
-#define ASSERT_EQUAL(a, b)
-#define ASSERT_NOT_EQUAL(a, b)
-#define ASSERT_STRING_NOT_EQUAL(a, b)
-
-#endif /* DEBUG_PRINT_ENABLED */
 
 #endif /* DEBUG_H_ */
